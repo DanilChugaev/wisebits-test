66948ae4c5ee0ea0987255f452cb2e03c33e727d
danilchugaev <dmchugaev@gmail.com>
2022-04-13 18:41:48 +0700

    upd container

--- coffee-feed/src/index.ts
+++ coffee-feed/src/index.ts
@@ -1,15 +1,36 @@
 import Card from './components/Card/index';
+import { ICard } from './components/Card/types';
 import './index.styl';
 
-const card = new Card();
+const card: ICard = new Card();
+let cardContainer: HTMLElement | null;
+let addButton: HTMLElement | null;
+
+/**
+ * Вставляет новую карточку в контейнер
+ */
+function insertNewCardIntoContainer(): void {
+  (cardContainer as HTMLDivElement).insertAdjacentHTML('beforeend', card.getComponent('новый компонент'));
+}
+
+/**
+ * Получает новую карточку с сервера и вставляет в контейнер
+ */
+function getNewCard(): void {
+  // тут делать запрос
+  insertNewCardIntoContainer();
+}
 
 /**
  * Стартует приложение после загрузки DOM
  */
 function startApp(): void {
-  const cardContainer = document.getElementById('container');
+  cardContainer = document.getElementById('container');
+  addButton = document.getElementById('button');
 
-  (cardContainer as HTMLDivElement).insertAdjacentHTML('beforeend', card.getComponent('новый компонент'));
+  getNewCard();
+
+  (addButton as HTMLButtonElement).addEventListener('click', getNewCard);
 }
 
 window.addEventListener('DOMContentLoaded', startApp);
